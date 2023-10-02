0f99731d0883e18e9188796b81e5625ffde0f121
fivifri <emelianova.kp@phystech.edu>
2023-10-02 19:58:53 +0500

    Task5

---
+++ Tyuiu.EmelianovaKP.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.EmelianovaKP.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual(6, ds.DistanceBetweenDots(1, 2, 5, 6));
+        }
+    }
+}
---
+++ Tyuiu.EmelianovaKP.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.EmelianovaKP.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint1.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #0 | Выполнил: Емельянова К. П. | ПКТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Емельянова К. П. | ПКТб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти расстояние между двумя точками с заданными координатами (x, y).   *");
+            Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x1, x2, y1, y2;
+
+            Console.WriteLine("x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("The distance is " + ds.DistanceBetweenDots(x1, y1, x2, y2));
+            Console.ReadKey();
+        }
+    }
+}

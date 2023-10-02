967080120b1e31799ab8a78de45a06822475e6ae
fivifri <emelianova.kp@phystech.edu>
2023-10-02 13:41:21 +0500

    Add project files.

---
+++ Tyuiu.EmelianovaKP.Sprint1.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.EmelianovaKP.Sprint1.Task0.V20.Lib
+{
+    public class DataService : ISprint1Task0V20
+    {
+        public double Calculate()
+        {
+            return 10 + 9 / 3;
+        }
+    }
+}
---
+++ Tyuiu.EmelianovaKP.Sprint1.Task0.V20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.EmelianovaKP.Sprint1.Task0.V20.Lib;
+namespace Tyuiu.EmelianovaKP.Sprint1.Task0.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #0 | Выполнил: Емельянова К. П. | ПКТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Емельянова К. П. | ПКТб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10+9/3 и печатает       *");
+            Console.WriteLine("* результат на экране.                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 10 + 9 / 3            ***************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}

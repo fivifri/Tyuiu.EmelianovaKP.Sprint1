fa858fb5b5fd1150556b7ef25b72545a2511b73a
fivifri <emelianova.kp@phystech.edu>
2023-10-02 17:46:36 +0500

    Task3

---
+++ Tyuiu.EmelianovaKP.Sprint1.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.EmelianovaKP.Sprint1.Task3.V17.Lib
+{
+    public class DataService : ISprint1Task3V17
+    {
+        public bool ZeroCheck(double number)
+        {
+            int tmp = (int)(number * 1000);
+            return (tmp % 10 == 0 || tmp % 100 / 10 == 0 || tmp % 1000 / 100 == 0);
+        }
+    }
+}
---
+++ Tyuiu.EmelianovaKP.Sprint1.Task3.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.EmelianovaKP.Sprint1.Task3.V17.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint1.Task3.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.ZeroCheck(4.012555));
+        }
+    }
+}
